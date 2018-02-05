a780d31f7fe5a7cdedf133dc48bb94b43b417bca
DanielHauge <Animcuil@gmail.com>
2018-02-05 19:48:39 +0100

    First Commit Test

---
+++ TriangleTest1/TriangleTest1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TriangleTest1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine(TriangleHandler.CalculateTriangleType(1,1,1).ToString());
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TriangleTest1/TriangleTest1/TriangleHandler.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TriangleTest1
+{
+    public class TriangleHandler
+    {
+
+        public enum TriangleType
+        {
+            Error,
+            Equilateral,
+            Isoceles,
+            Scalene
+
+        }
+
+        public static TriangleType CalculateTriangleType(int a, int b, int c)
+        {
+            
+            if (!(a > 0 && b > 0 && c > 0))
+            {
+                return TriangleType.Error;
+            }
+            else if (a==b && b == c)
+            {
+                return TriangleType.Equilateral;
+            }
+            else if (a == b || b == c || a == c)
+            {
+                return TriangleType.Isoceles;
+            }
+            else
+            {
+                return TriangleType.Scalene;
+            }
+        }
+    }
+}
